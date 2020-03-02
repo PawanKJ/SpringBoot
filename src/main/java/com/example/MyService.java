502b62b0e824c9789415b1c5f6c4bae4d785a5e3
Pawan Jha <PawanKJ@users.noreply.github.com>
2020-03-03 00:28:42 +0530

    added method parameter to request mapping

--- src/main/java/com/example/MyService.java
+++ src/main/java/com/example/MyService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyService {
 
-	@RequestMapping("/Hello")
+	@RequestMapping("/Hello", method = GET)
 	public String hello()
 	{
 		return "Hello";
